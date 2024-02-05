8307bea1b74b8a56b57d7e9bbc17013fa2260838
ReynardHansel <92787298+ReynardHansel@users.noreply.github.com>
2024-02-05 22:42:34 +0700

    Finished <Button /> and <ProfileImage />
    + added images domains of cdn.discordapp.com in next.config

--- src/app/_components/NewTweetForm.tsx
+++ src/app/_components/NewTweetForm.tsx
@@ -1,14 +1,22 @@
 import Button from "./Button";
 import ProfileImage from "./ProfileImage";
+import { getServerAuthSession } from "~/server/auth";
 
-export default function NewTweetForm() {
+export default async function NewTweetForm() {
+  const session = await getServerAuthSession();
+  console.log(session);
+  if (!session) return;
+
   return (
     <form className="flex flex-col gap-2 border-b px-4 py-2">
       <div className="flex gap-4">
-        <ProfileImage />
-        <textarea className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none" placeholder="What's happening?" />
+        <ProfileImage src={session.user.image} />
+        <textarea
+          className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
+          placeholder="What's happening?"
+        />
       </div>
-      <Button>Tweet</Button>
+      <Button className="self-end">Tweet</Button>
     </form>
-  )
+  );
 }
--- src/app/_components/ProfileImage.tsx
+++ src/app/_components/ProfileImage.tsx
@@ -1,7 +1,17 @@
-export default function ProfileImage() {
-  return (
-    <div>
-        This is the profile image
-    </div>
-  )
+import Image from "next/image"
+
+type ProfileImageProps = {
+  src?: string | null
+  className?: string
+}
+
+export default function ProfileImage({
+  src,
+  className = "",
+}: ProfileImageProps) {
+  return <div
+    className={`relative h-12 w-12 overflow-hidden rounded-full ${className}`}
+  >
+    {src == null ? null : <Image src={src} alt="Profile Image" quality={100} fill />}
+  </div>
 }
