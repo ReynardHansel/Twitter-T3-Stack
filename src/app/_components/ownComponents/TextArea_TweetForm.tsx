42eee8fd99abefac1a135baa3148a56ce92db1f1
ReynardHansel <92787298+ReynardHansel@users.noreply.github.com>
2024-02-07 22:03:43 +0700

    <TextArea_TweetForm/> frontend finished

--- src/app/_components/ownComponents/TextArea_TweetForm.tsx
+++ src/app/_components/ownComponents/TextArea_TweetForm.tsx
@@ -1,29 +1,35 @@
-'use client'
-import { useState, useLayoutEffect, useRef } from 'react'
+"use client";
+import { useState, useLayoutEffect, useRef, useCallback } from "react";
 
-function updateTextAreaSize(textArea?: HTMLTextAreaElement){
-    if (textArea == null) return
+function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
+  if (textArea == null) return;
 
-    textArea.style.height = "0"
-    textArea.style.height = `${textArea.scrollHeight}px`
+  textArea.style.height = "0";
+  textArea.style.height = `${textArea.scrollHeight}px`;
 }
 
 export default function TextArea_TweetForm() {
-    const [inputValue, setInputValue] = useState("")
-    const textAreaRef = useRef<HTMLTextAreaElement>(null)
+  const [inputValue, setInputValue] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>();
 
-    useLayoutEffect(() => {
-        updateTextAreaSize(textAreaRef.current)
-    }, [inputValue])
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updateTextAreaSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
+  //? [] means it's only going to be called 1 time (when the textArea is called/made)
+
+  useLayoutEffect(() => {
+    updateTextAreaSize(textAreaRef.current);
+  }, [inputValue]);
 
   return (
     <textarea
-    ref={textAreaRef}
-    style={{ height: 0 }}
-    value={inputValue}
-    onChange={e => setInputValue(e.target.value)}
-    className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
-    placeholder="What's happening?"
+      ref={inputRef}
+      style={{ height: 0 }}
+      value={inputValue}
+      onChange={(e) => setInputValue(e.target.value)}
+      className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
+      placeholder="What's happening?"
     />
-)
+  );
 }
