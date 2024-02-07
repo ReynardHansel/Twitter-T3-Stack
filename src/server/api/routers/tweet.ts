4891f9f0031548bfbce4afa7087e7bad554b4795
ReynardHansel <92787298+ReynardHansel@users.noreply.github.com>
2024-02-07 22:36:24 +0700

    modified router's and root.ts  default code

---
+++ src/server/api/routers/tweet.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+export const tweetRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(z.object({ content: z.string() }))
+    .mutation(async ({ input: { content }, ctx }) => {
+      const tweet = await ctx.prisma.tweet.create({
+        data: { content, userId: ctx.session.user.id },
+      });
+
+      return tweet
+    }),
+});
+
+//* Notes:
+//* - protectedProcedure = must be authenticated to be able to do what's inside the router (create tweet)
+//* ctx: context variable
+//* .mutation allows data manipulation
