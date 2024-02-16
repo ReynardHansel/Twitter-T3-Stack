2beac333c91f9d907f0e3eac477eb9bc88077d31
ReynardHansel <92787298+ReynardHansel@users.noreply.github.com>
2024-02-16 17:51:07 +0700

    An attempt kinda worked by making the <Form /> it's own component (have not been fully tested and continued yet), try to receive the props needed for <Form /> next

--- src/app/_components/NewTweetForm.tsx
+++ src/app/_components/NewTweetForm.tsx
@@ -1,15 +1,16 @@
-'use client'
+// 'use client'
 
 import Button from "./Button";
 import ProfileImage from "./ProfileImage";
 import { getServerAuthSession } from "~/server/auth";
 import TextArea_TweetForm from "./ownComponents/TextArea_TweetForm";
-import { api } from "~/trpc/react";
+import Form from "./ownComponents/Form";
+// import { api } from "~/trpc/react";
 // import { api } from "./../../trpc/server";
 
 export default async function NewTweetForm() {
   // const createTweet = api.tweet.create.mutate()
-  const createTweet = api.tweet.create.useMutation()
+  // const createTweet = api.tweet.create.useMutation()
   const session = await getServerAuthSession();
   // console.log(session);
 
@@ -26,12 +27,14 @@ export default async function NewTweetForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-2 border-b px-4 py-2">
-      <div className="flex gap-4">
-        <ProfileImage src={session.user.image} />
-        <TextArea_TweetForm />
-      </div>
-      <Button className="self-end">Tweet</Button>
-    </form>
+    // <form onSubmit={handleSubmit} className="flex flex-col gap-2 border-b px-4 py-2">
+    //   <div className="flex gap-4">
+    //     <ProfileImage src={session.user.image} />
+    //     <TextArea_TweetForm />
+    //   </div>
+    //   <Button className="self-end">Tweet</Button>
+    // </form>
+
+    <Form />
   );
 }
--- src/app/_components/ownComponents/Form.tsx
+++ src/app/_components/ownComponents/Form.tsx
@@ -3,13 +3,17 @@
 import ProfileImage from "../ProfileImage";
 import TextArea_TweetForm from "./TextArea_TweetForm";
 import Button from "../Button"
+import { api } from "~/trpc/react";
 
-export default function Form({ onSubmit, session}:any) {
+export default function Form() {
+  const createTweet = api.tweet.create.useMutation()
+
   return (
-    <form onSubmit={onSubmit} className="flex flex-col gap-2 border-b px-4 py-2">
+    <form className="flex flex-col gap-2 border-b px-4 py-2">
       <div className="flex gap-4">
-        <ProfileImage src={session.user.image} />
+        {/* <ProfileImage src={session.user.image} /> */}
         <TextArea_TweetForm />
+        <p>Something changed</p>
       </div>
       <Button className="self-end">Tweet</Button>
     </form>
