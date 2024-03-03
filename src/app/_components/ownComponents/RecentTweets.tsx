5a37cec8ee5f598a8fb6f25ba621a79fcc5550bd
Reynard Hansel <92787298+ReynardHansel@users.noreply.github.com>
2024-03-03 09:40:14 +0700

    Tweets mapping succeeded and tweets are displayed successfully
    <RecentTweets /> component is created and used in the <InfiniteTweetList /> component to display the tweets
    <RecentTweets /> made to be it's own component to implement client side functions from tutorial

--- src/app/_components/ownComponents/RecentTweets.tsx
+++ src/app/_components/ownComponents/RecentTweets.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { isError } from "@tanstack/react-query";
 import { InfiniteTweetList } from "../InfiniteTweetList";
 import { api } from "~/trpc/react";
 
@@ -9,5 +10,13 @@ export function RecentTweets() {
     { getNextPageParam: (lastPage) => lastPage.nextCursor },
   );
 
-  return <InfiniteTweetList tweets={tweets} />;
+  return (
+    <InfiniteTweetList
+      tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
+      isError={tweets.isError}
+      isLoading={tweets.isLoading}
+      hasMore={tweets.hasNextPage}
+      fetchNewTweets={tweets.fetchNextPage}
+    />
+  );
 }
