79ae7ec2b475d1ac3bff2952fac6696a20f99a8a
Reynard Hansel <92787298+ReynardHansel@users.noreply.github.com>
2024-03-11 11:41:05 +0700

    Added default fallback of VscAccount img/svg component to ProfilePicture is the user haven't set one (in <ProfileImage />)

--- src/app/_components/ownComponents/LogoutButton.tsx
+++ src/app/_components/ownComponents/LogoutButton.tsx
@@ -9,9 +9,7 @@ export default function LogoutButton() {
       <IconHoverEffect>
         <span className="flex items-center gap-4">
           <VscSignOut className="h-8 w-8 fill-red-700" />
-          <span className="hidden text-lg text-red-700 md:inline">
-            Log Out
-          </span>
+          <span className="hidden text-lg text-red-700 md:inline">Log Out</span>
         </span>
       </IconHoverEffect>
     </button>
